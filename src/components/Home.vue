c0b34c9cb565894d9070ac8dc18217bd6bb7f39b
andrured <andrured32@gmail.com>
2025-05-06 23:42:26 -0500

    sadasdasd

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <h1>HOla mundo</h1>
-    <h1>HOla</h1>
-    ..........
-    <p>Test para Github</p>
+    <h>Mi primera pagina web</h>
+    <h3>Que hace loka</h3>
+    <h3>Que hace lokax2</h3>
+
+    <p>pipe</p>
+    
+    <h1>jiji+</h1>
 </template>
