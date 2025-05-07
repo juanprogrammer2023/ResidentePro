344aa9c8c0dcf19d4c6eba16de7b859667a56840
Juan Pablo Pineda Madrigal <juanpp282@gmail.com>
2025-05-06 23:43:44 -0500

    Nuevo Cambio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,4 @@
 <template>
     <h1>HOla mundo</h1>
     <h>Mi primera pagina web</h>
-    <h3>Que hace loka</h3>
-    <h3>Que hace lokax2</h3>
-
-    <p>pipe</p>
-    
-    <h1>jiji+</h1>
 </template>
