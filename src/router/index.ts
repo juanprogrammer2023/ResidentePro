31067453c165e6a2019dc645a083adb4ef0bd775
Juan Pablo Pineda Madrigal <juanpp282@gmail.com>
2025-05-07 00:57:21 -0500

    login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/folder/:id',
     component: () => import ('../views/FolderPage.vue')
+  },
+  {
+    path: '/login',
+    component: () => import ('../views/LoginPage.vue')
   }
 ]
 
